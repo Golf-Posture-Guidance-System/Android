426eeb56ab614dadde927c02954c6e7c2a0189f3
yerim <yerim343@naver.com>
2021-03-25 01:24:28 +0900

    login and join xml

---
+++ app/src/main/java/com/example/golf/submain.java
@@ -0,0 +1,14 @@
+package com.example.golf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class submain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submain);
+    }
+}
