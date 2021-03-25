f5a42bbad4b8f5b450abc33f958af59ebe2d8593
jaewon <m970314@naver.com>
2021-03-25 21:08:56 +0900

    asd

--- app/src/main/java/com/example/golf/splash.java
+++ app/src/main/java/com/example/golf/splash.java
@@ -2,13 +2,27 @@ package com.example.golf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 
 public class splash extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Handler mHandler = new Handler();
         setContentView(R.layout.activity_splash);
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(splash.this,submain.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },1000);
+
+
     }
 }
