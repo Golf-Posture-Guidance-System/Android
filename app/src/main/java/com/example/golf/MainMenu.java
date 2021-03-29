c73b77357942a58495623d5a06f5aed9fea8f819
yerim <yerim343@naver.com>
2021-03-29 16:58:59 +0900

    main menu

--- app/src/main/java/com/example/golf/MainMenu.java
+++ app/src/main/java/com/example/golf/MainMenu.java
@@ -1,8 +1,13 @@
 package com.example.golf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.navBtn).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setItemIconTintList(null);
     }
 }
