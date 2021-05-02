28e187d976c66cedcfaab832b287490569ff8e44
yerim <yerim343@naver.com>
2021-05-03 08:55:37 +0900

    tip comment

--- app/src/main/java/com/example/golf/analysis_detail.java
+++ app/src/main/java/com/example/golf/analysis_detail.java
@@ -16,20 +16,29 @@ public class analysis_detail extends AppCompatActivity implements View.OnClickLi
             R.id.Btn1,R.id.Btn2,R.id.Btn3,R.id.Btn4,
             R.id.Btn5,R.id.Btn6,R.id.Btn7};
 
-    private TextView commentText;
+    private TextView address, takeAway, top, down, impact, followThru, finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_detail);
 
+        address = (TextView)findViewById(R.id.commentText);
+        takeAway = (TextView)findViewById(R.id.commentText);
+        top = (TextView)findViewById(R.id.commentText);
+        down = (TextView)findViewById(R.id.commentText);
+        impact = (TextView)findViewById(R.id.commentText);
+        followThru = (TextView)findViewById(R.id.commentText);
+        finish = (TextView)findViewById(R.id.commentText);
+
         pracBtn = (Button)findViewById(R.id.pracNoteBtn);
         mainBtn = (Button)findViewById(R.id.backToMainBtn);
+
         for ( int i=0;i<7;i++) {
             poseBtn[i] = (Button) findViewById(RidBtn[i]);
             poseBtn[i].setOnClickListener(this);
         }
-        commentText = (TextView)findViewById(R.id.commentText);
+
         pracBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,15 +58,25 @@ public class analysis_detail extends AppCompatActivity implements View.OnClickLi
     public void onClick(View view){
         switch (view.getId()){
             case R.id.Btn1:
-                //이미지 뷰를 어드래스 이미지로 표기
-                //텍스트를 해당 버튼에 맞게 수정.
-                commentText.setText("지피지기팁:어드래스삼각형어쩌구.");
+                address.setText(R.string.address);
                 break;
             case R.id.Btn2:
-                //이미지 뷰를 어드래스 이미지로 표기
-                commentText.setText("지피지기팁:좋은 백스윙은 채가 일자가 되어야합니다.");
+                takeAway.setText(R.string.takeAway);
                 break;
             case R.id.Btn3:
+                top.setText(R.string.top);
+                break;
+            case R.id.Btn4:
+                down.setText(R.string.down);
+                break;
+            case R.id.Btn5:
+                impact.setText(R.string.impact);
+                break;
+            case R.id.Btn6:
+                followThru.setText(R.string.followThru);
+                break;
+            case R.id.Btn7:
+                finish.setText(R.string.finish);
                 break;
         }
     }
