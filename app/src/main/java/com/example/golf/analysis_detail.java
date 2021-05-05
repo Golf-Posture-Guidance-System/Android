20c280ad8173e763f7e8ec153c99e1b593ae2140
m970314 <63191420+m970314@users.noreply.github.com>
2021-05-06 06:13:34 +0900

    asd
    asd

--- app/src/main/java/com/example/golf/analysis_detail.java
+++ app/src/main/java/com/example/golf/analysis_detail.java
@@ -3,6 +3,8 @@ package com.example.golf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+
 public class analysis_detail extends AppCompatActivity implements View.OnClickListener{
     private Button pracBtn, mainBtn;
     private Button poseBtn[] = new Button[7];
@@ -18,17 +22,18 @@ public class analysis_detail extends AppCompatActivity implements View.OnClickLi
             R.id.Btn1,R.id.Btn2,R.id.Btn3,R.id.Btn4,
             R.id.Btn5,R.id.Btn6,R.id.Btn7};
     private ImageView pose_img;
-
+    Intent intent;
+    String userid,imagename;
+    File f;
+    Bitmap myBitmap;
+    ImageView imageView;
     private TextView address, takeAway, top, down, impact, followThru, finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_detail);
-
-        pose_img = (ImageView)findViewById(R.id.poseimg);
-        pose_img.setImageResource(R.drawable.g0);
-
+        imageView = (ImageView)findViewById(R.id.poseimg);
         address = (TextView)findViewById(R.id.commentText);
         takeAway = (TextView)findViewById(R.id.commentText);
         top = (TextView)findViewById(R.id.commentText);
@@ -36,7 +41,6 @@ public class analysis_detail extends AppCompatActivity implements View.OnClickLi
         impact = (TextView)findViewById(R.id.commentText);
         followThru = (TextView)findViewById(R.id.commentText);
         finish = (TextView)findViewById(R.id.commentText);
-
         pracBtn = (Button)findViewById(R.id.pracNoteBtn);
         mainBtn = (Button)findViewById(R.id.backToMainBtn);
 
@@ -64,31 +68,60 @@ public class analysis_detail extends AppCompatActivity implements View.OnClickLi
     public void onClick(View view){
         switch (view.getId()){
             case R.id.Btn1:
-                pose_img.setImageResource(R.drawable.g0);
+                intent = getIntent() ;
+                userid = ((login)login.context_main).userid;
+                imagename =  intent.getStringExtra("imagename") ;
+                f = new File("/sdcard/" + userid, imagename + '0' + ".jpg");
+                myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
                 address.setText(R.string.address);
                 break;
             case R.id.Btn2:
-                pose_img.setImageResource(R.drawable.g1);
+                intent = getIntent() ;
+                userid = ((login)login.context_main).userid;
+                imagename =  intent.getStringExtra("imagename") ;
+                f = new File("/sdcard/" + userid, imagename + '1' + ".jpg");
+                myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
                 takeAway.setText(R.string.takeAway);
                 break;
             case R.id.Btn3:
-                pose_img.setImageResource(R.drawable.g2);
+                intent = getIntent() ;
+                userid = ((login)login.context_main).userid;
+                imagename =  intent.getStringExtra("imagename") ;
+                f = new File("/sdcard/" + userid, imagename + '2' + ".jpg");
+                myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
                 top.setText(R.string.top);
                 break;
             case R.id.Btn4:
-                pose_img.setImageResource(R.drawable.g3);
+                intent = getIntent() ;
+                userid = ((login)login.context_main).userid;
+                imagename =  intent.getStringExtra("imagename") ;
+                f = new File("/sdcard/" + userid, imagename + '3' + ".jpg");
+                myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
                 down.setText(R.string.down);
                 break;
             case R.id.Btn5:
-                pose_img.setImageResource(R.drawable.g4);
+                intent = getIntent() ;
+                userid = ((login)login.context_main).userid;
+                imagename =  intent.getStringExtra("imagename") ;
+                f = new File("/sdcard/" + userid, imagename + '4' + ".jpg");
+                myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
                 impact.setText(R.string.impact);
                 break;
             case R.id.Btn6:
-                pose_img.setImageResource(R.drawable.g5);
                 followThru.setText(R.string.followThru);
                 break;
             case R.id.Btn7:
-                pose_img.setImageResource(R.drawable.g7);
+                intent = getIntent();
+                userid = ((login)login.context_main).userid;
+                imagename =  intent.getStringExtra("imagename") ;
+                f = new File("/sdcard/" + userid, imagename + '5' + ".jpg");
+                myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
                 finish.setText(R.string.finish);
                 break;
         }
