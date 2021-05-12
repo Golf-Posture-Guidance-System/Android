670ff1d66a3477747e278d032ce7cbbb7b9b208f
m970314 <63191420+m970314@users.noreply.github.com>
2021-05-12 18:39:48 +0900

    카메라 시작배경
    카메라 시작배경

--- app/src/main/java/com/example/golf/swing_record.java
+++ app/src/main/java/com/example/golf/swing_record.java
@@ -11,156 +11,119 @@ import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Timer;
 
 public class swing_record extends AppCompatActivity implements SurfaceHolder.Callback {
-
     CountDownTimer swingTimer, readyTimer;
     TextView time_text, ready_text;
     ProgressBar progressBar;
+    public static Context context_main;
     int i = 0;
     final int READYTIME = 4000;
     final int TOTALTIME = 30000;
     final int COUNT_DOWN_INTERVAL = 1000;
-
-    private Camera mcam;
+    private Camera camera;
     private MediaRecorder mediaRecorder;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
-
-    boolean recording = false; //초기는 녹화중이 아님
     String filepath;
-
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        if (mcam == null) {
-            try {
-
-                mcam.setPreviewDisplay(surfaceHolder);
-                mcam.startPreview();
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-        if (surfaceHolder.getSurface() == null) {
-            // preview surface does not exist
-            return;
-        }
-        // stop preview before making changes
-        try {
-            mcam.stopPreview();
-        } catch (Exception e) {
-            // ignore: tried to stop a non-existent preview
-        }
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-        // start preview with new settings
-        try {
-            mcam.setPreviewDisplay(surfaceHolder);
-            mcam.startPreview();
-        } catch (Exception e) {
-        }
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) { }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swing_record);
-
-        setting();
-
+        final MediaController mediaController =
+                new MediaController(this);
+        TedPermission.with(this)
+                .setPermissionListener(permission)
+                .setRationaleMessage("녹화를 위하여 권한을 허용해주세요.")
+                .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
+                .check();
         final ToggleButton tbtn = (ToggleButton) this.findViewById(R.id.recordBtn);
 
         tbtn.setOnClickListener(v -> {
-            if (recording) { //녹화 중일때 버튼 클릭시 녹화 종료
-                mediaRecorder.stop();
-                mediaRecorder.release();//stop release세트임
-                mediaRecorder = null;
-                mcam.lock();
-                recording = false;
-                setResult(RESULT_OK);
-                Intent intent = new Intent();
-                intent.putExtra("filepath", filepath);
-                setResult(RESULT_OK,intent);
-                finish();
-            }
-            else { //화면 녹화 시작
+
+
+            if (tbtn.isChecked()) { //화면 녹화 시작
                 readyTimer();
-                runOnUiThread (new Runnable() {
+                runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        tbtn.setBackgroundDrawable( //버튼바꾸기
+                        tbtn.setBackgroundDrawable(
                                 getResources().getDrawable(R.drawable.ic_stop)
                         );
-                        long now = System.currentTimeMillis();
-                        Date mDate = new Date(now);
-                        SimpleDateFormat simpleDate = new SimpleDateFormat("yyyyMMdd_hhmmss");
-                        String getTime = simpleDate.format(mDate);
                         Toast.makeText(swing_record.this, "녹화가 시작되었습니다.", Toast.LENGTH_SHORT).show();
-                        try {
-                            filepath = "sdcard/DCIM/" + getTime + ".mp4";
-                            mediaRecorder = new MediaRecorder();
-                            mcam.unlock();
-                            mediaRecorder.setCamera(mcam);
-                            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-                            mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
-                            mediaRecorder.setOrientationHint(90);
-                            mediaRecorder.setOutputFile("sdcard/" + getTime + ".mp4");
-                            mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
-                            mediaRecorder.prepare();
-                            mediaRecorder.start();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            mediaRecorder.release();
-                        }
-                        recording = true;
+
                     }
                 });
+
+            } else {//화면 녹화 중지
+                mediaRecorder.stop();
+
+                        Intent intent = new Intent();
+                        intent.putExtra("filepath", filepath);
+                        setResult(RESULT_OK,intent);
+                        finish();
+
             }
         });
-    }
-
-    private  void setting(){
-        mcam = Camera.open();
-
-        Camera.Parameters params = mcam.getParameters();
-        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-        mcam.setParameters(params);
-
-        mcam.setDisplayOrientation(90);
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     PermissionListener permission = new PermissionListener() {
         @Override
         public void onPermissionGranted() {
             Toast.makeText(swing_record.this, "권한 허가", Toast.LENGTH_SHORT).show();
+
+            camera = android.hardware.Camera.open();
+            camera.setDisplayOrientation(90);
+            Camera.Parameters params = camera.getParameters();
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            camera.setParameters(params);
+
+            surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+            surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.addCallback(swing_record.this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            try {
+                long now = System.currentTimeMillis();
+                Date mDate = new Date(now);
+                SimpleDateFormat simpleDate = new SimpleDateFormat("yyyyMMdd_hhmmss");
+                String getTime = simpleDate.format(mDate);
+                filepath = "sdcard/DCIM/" + getTime + ".mp4";
+                mediaRecorder = new MediaRecorder();
+                camera.unlock();
+                mediaRecorder.setCamera(camera);
+                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+                mediaRecorder.setOrientationHint(90);
+                mediaRecorder.setOutputFile(filepath);
+                mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+                mediaRecorder.prepare();
+                mediaRecorder.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+                mediaRecorder.release();
+            }
         }
 
         @Override
@@ -168,8 +131,6 @@ public class swing_record extends AppCompatActivity implements SurfaceHolder.Cal
             Toast.makeText(swing_record.this, "권한 거부", Toast.LENGTH_SHORT).show();
         }
     };
-
-
     public  void readyTimer() {
         ready_text = (TextView) findViewById(R.id.readyBtn);
         readyTimer = new CountDownTimer(READYTIME, COUNT_DOWN_INTERVAL) {
@@ -188,7 +149,7 @@ public class swing_record extends AppCompatActivity implements SurfaceHolder.Cal
             }
         }.start();
     }
-    public void swingTimer() {
+        public void swingTimer() {
             time_text = (TextView) findViewById(R.id.countDown);
             progressBar = (ProgressBar) findViewById(R.id.progress);
             progressBar.setProgress(i);
@@ -208,7 +169,36 @@ public class swing_record extends AppCompatActivity implements SurfaceHolder.Cal
                 }
             }.start();
         }
+    private void refreshCamera(android.hardware.Camera camera) {
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
+
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        setCamera(camera);
+    }
     private void setCamera(android.hardware.Camera cam) {
-        mcam = cam;
+
+        camera = cam;
+
+    }
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+        refreshCamera(camera);
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
     }
 }
